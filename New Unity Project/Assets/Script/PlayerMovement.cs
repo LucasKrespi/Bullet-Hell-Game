21269c55466e51fc9d69baa958b36bb05fe42c27
lucaskrespi <lucas.krespi@gmail.com>
2021-09-10 17:04:53 -0400

    Terrain creation/space ship mesh changed

---
+++ New Unity Project/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Vector3 m_MousePos;
+    Vector2 m_Movement;
+    Rigidbody2D m_ShipRB;
+    private float m_speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ShipRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceMouse();
+        moveShip();
+    }
+
+    private void FixedUpdate()
+    {
+        m_ShipRB.MovePosition(m_ShipRB.position + m_Movement * m_speed * Time.fixedDeltaTime);
+    }
+    void faceMouse()
+    {
+        m_MousePos = Input.mousePosition;
+        m_MousePos = Camera.main.ScreenToWorldPoint(m_MousePos);
+
+        Vector2 characterDirection = new Vector2(m_MousePos.x - transform.position.x, m_MousePos.y - transform.position.y);
+
+        transform.up = characterDirection;
+    }
+
+    void moveShip()
+    {
+        m_Movement.x = Input.GetAxisRaw("Horizontal");
+        m_Movement.y = Input.GetAxisRaw("Vertical");
+    }
+}
---
+++ New Unity Project/Assets/Script/PlayerShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    public Transform FirePoint;
+    public GameObject BulletPrefab;
+
+    private float bulletForce = 10; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        GameObject bullet = Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
+        Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
+        bulletRB.AddForce(FirePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
